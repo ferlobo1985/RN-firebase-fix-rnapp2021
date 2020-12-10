e3fa9edfdc361338786a560256bb4bebf537faad
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-12-09 23:49:01 -0300

    fixes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-import 'firebase/database';
+// import 'firebase/database';
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPoohVlcDC5bCjGDoOSKy8dYhl91TFOYY",
-  authDomain: "rnapp2021.firebaseapp.com",
-  projectId: "rnapp2021",
-  databaseURL: "https://rnapp2021-default-rtdb.firebaseio.com",
-  storageBucket: "rnapp2021.appspot.com",
-  messagingSenderId: "440207308793",
-  appId: "1:440207308793:web:16f71c5df88aa500316f28",
-  measurementId: "G-NKE8VSJDH4"
+    apiKey: "AIzaSyAe25ytPIPrcov-KTMhtmHFZNiwDtDDNYs",
+    authDomain: "rnapp2021-e42ca.firebaseapp.com",
+    projectId: "rnapp2021-e42ca",
+    storageBucket: "rnapp2021-e42ca.appspot.com",
+    messagingSenderId: "1086782046013",
+    appId: "1:1086782046013:web:72aad6643601a62b060f54",
+    measurementId: "G-8ZMCK1RGCE"
 };
 
 if (!firebase.apps.length) {
@@ -19,7 +18,12 @@ if (!firebase.apps.length) {
   // firebase.analytics();
 }
 
-// const DB = firebase.database();
-// const usersCollection = DB.ref("users");
 
-export { firebase};
+const db = firebase.firestore().settings({ experimentalForceLongPolling: true })
+const usersCollection = firebase.firestore().collection('users');
+
+
+export {
+  firebase,
+  usersCollection
+};
--- src/store/api/index.js
+++ src/store/api/index.js
@@ -1,7 +1,23 @@
 import { firebase, usersCollection } from '../../firebase';
-import "firebase/firestore";
+
 
 export const registerUser =  async ({ email,password }) =>{
+    try{
+        const response = await firebase.auth()
+        .createUserWithEmailAndPassword(email,password);
+
+        const {user} = response;
+        const userProfile = {
+            uid: user.uid,
+            email: email
+        };
+        await usersCollection.doc(user.uid).set(userProfile);
+        return { isAuth: true, user:userProfile }
+    }catch(error){
+        return { error:error.message }
+    }
+
+
     // firebase.auth()
     // .createUserWithEmailAndPassword(email,password)
     // .then( (response)=>{
@@ -29,24 +45,24 @@ export const registerUser =  async ({ email,password }) =>{
        
     // })
 
-    try{
-        const response = await firebase.auth()
-        .createUserWithEmailAndPassword(email,password);
+    // try{
+    //     const response = await firebase.auth()
+    //     .createUserWithEmailAndPassword(email,password);
 
-        const {user} = response;
-        const DB = firebase.database();
-        const userProfile = {
-            uid: user.uid,
-            email: email
-        };
-        await DB.ref("users/" + user.uid).set(userProfile)
-        return { 
-            isAuth: true, 
-            user:userProfile
-         }
-    }catch(error){
-        return { error:error.message }
-    }
+    //     const {user} = response;
+    //     const DB = firebase.database();
+    //     const userProfile = {
+    //         uid: user.uid,
+    //         email: email
+    //     };
+    //     await DB.ref("users/" + user.uid).set(userProfile)
+    //     return { 
+    //         isAuth: true, 
+    //         user:userProfile
+    //      }
+    // }catch(error){
+    //     return { error:error.message }
+    // }
 }
 
 const readData = ()=>{
